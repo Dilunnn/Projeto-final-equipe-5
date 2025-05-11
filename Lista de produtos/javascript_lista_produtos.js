0f771d9cb9aefbe1bd92c4e519b30bdb1858c9f7
rodrigo viana <151778963+Dilunnn@users.noreply.github.com>
2025-05-11 18:51:15 -0300

    programção da página em tela maiores que 768px e deixando responsivo

--- Lista de produtos/javascript_lista_produtos.js
+++ Lista de produtos/javascript_lista_produtos.js
@@ -1,5 +1,5 @@
  function produtosHTML(tela) {
-                    if (tela <= 360) {
+                    if (tela <= 576) {
                     for (let i = 1; i < 8; i++) {
                     let produtoDiv = document.createElement("div");
                     produtoDiv.setAttribute("id",`produto${i}`)
@@ -10,7 +10,7 @@
                     
 
                     produtoDiv.innerHTML = `
-                                <div id="imagem-produto" style="background-color: #ffff;">
+                                <div id="imagem-produto" class="text-center">
                                     <img src="Produto gênerico.png" alt="Produto gênerico">
                                 </div>
                                 <div id="descrição-produto">
@@ -25,7 +25,33 @@
                     
                     produtos.appendChild(produtoDiv);
                 }
-            } 
+            } else {
+                for (let i = 1; i < 16; i++) {
+                    let produtoDiv = document.createElement("div");
+                    produtoDiv.setAttribute("id",`produto${i}`)
+                    produtoDiv.setAttribute("class", "col-6 col-md-3");
+                    produtoDiv.addEventListener('click',() => {
+                        window.location.href = '/detalhes.html'
+                    })
+                    
+
+                    produtoDiv.innerHTML = `
+                                <div id="imagem-produto" class="text-center">
+                                    <img src="Produto gênerico.png" alt="Produto gênerico">
+                                </div>
+                                <div id="descrição-produto">
+                                    <p>Tênis</p>
+                                    <p>K-Swiss V8 - Mascu...</p>
+                                    <div class="d-flex justify-content-between">
+                                        <p id="preco-antigo" class="text-decoration-line-through opacity-50">R$200</p>
+                                    <p id="preco-atual" class="fw-bold">R$100</p>
+                                </div>
+                                </div>
+                            `;
+                    
+                    produtos.appendChild(produtoDiv);
+                }
+            }
             // adicionar 15 produtos se for maior
         }
         const produtos = document.getElementById("produtos");
